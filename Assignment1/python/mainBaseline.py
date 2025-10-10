1b5e71fd20fe6c32e9176b34c6743b5c5c68beca
yuliu <liamliu@yeah.net>
2025-10-10 07:39:49 +0000

    change

--- Assignment1/python/mainBaseline.py
+++ Assignment1/python/mainBaseline.py
@@ -19,24 +19,22 @@ for testId in range(4):
     data = load_datadir_re(datadir, bitdepth, resize, gamma)
     data.name = dataNameStack[testId]  # 设置数据集名称
 
-    # # 1.2.1  Least Squares-Based Method
-    # Normal = myPMS(data, resize)
-    # #  save Normal mat and png file to results folder
-    # print(f"Normal map for {dataNameStack[testId]} saved.")
+    # 1.2.1  Least Squares-Based Method
+    Normal = myPMS(data, resize)
+    # Save Normal mat and png file to results folder
+    print(f"Normal map for {dataNameStack[testId]} saved.")
 
     # 1.2.1  Robust Photometric Stereo
     Normal = myRobustPMS(data,resize)
-    #  save Normal mat and png file to results folder
+    # Save Normal mat and png file to results folder
     
     # 1.2.2 PCA-Based Method
-    # Normal = myPCA(data, resize)
-    #  save Normal mat and png file to results folder
+    Normal = myPCA(data, resize)
+    # Save Normal mat and png file to results folder
 
-    # # 1.2.3 Calculate the depth map and convert it into mesh
-    # pc = mynormal2depth(Normal, data)
-    # 1.2.3 Convert PC to mesh
+    # 1.2.3 Calculate the depth map and convert it into mesh
     depth_map, mesh = mynormal2depth(Normal, data)
-    # 1.2.3 Save results mesh in standard mesh format
+    # Save results mesh in standard mesh format
     depth_map_path, mesh_path = save_results(depth_map, mesh, data, "pms")
     print("Mesh statistics:")
     print(f"Number of vertices: {len(mesh.vertices)}")
